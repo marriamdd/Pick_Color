66b8780e844dc3866e8cee21d0e9fc8d5c58b27d
marriamdd <davitashvili.m3@gmail.com>
2024-09-01 22:40:56 +0400

    style:Implement design features

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,39 @@
 import { useState } from "react";
 import "./App.css";
+import "./index.css";
 
 function App() {
   const [hex, setHex] = useState("#000000");
+
   const handleHexChange = (e) => {
-    setHex(e);
+    setHex(e.target.value);
   };
+
   return (
     <>
       <div
         style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          gap: "1rem",
-          padding: "2rem",
           border: `2px solid ${hex}`,
-          borderRadius: "0.8rem",
         }}
+        className="flex flex-col  items-center justify-between w-[50vw] h-[50vh] rounded-[0.8rem]"
       >
         <input
           style={{
-            border: `2px solid ${hex}`,
+            borderColor: hex,
           }}
+          className="mt-[4rem] rounded-[3px] w-[30vw] border-[2px]"
           type="color"
-          onChange={(e) => {
-            handleHexChange(e.target.value);
-          }}
+          onChange={handleHexChange}
           value={hex}
         />
         <input
           style={{
-            border: `2px solid ${hex}`,
+            borderColor: hex,
           }}
+          className="mb-[4rem] text-[1.6rem] border-[2px] rounded-[3px] text-center"
           type="text"
           value={hex}
+          onChange={handleHexChange}
         />
       </div>
     </>
